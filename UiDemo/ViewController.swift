b6dd9066433d42ff58802b18696f5ccc752d7365
Rathod125 <tusharrathod5801@gmail.com>
2023-09-16 12:44:32 -0400

    Initial Commit

---
+++ UiDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiDemo
+//
+//  Created by user234887 on 9/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
